21472cdaa5733b00ed95cb9ef660f01ffc7774e1
LG West <larsgwest@gmail.com>
2020-05-15 07:08:18 +0200

    prel add

---
+++ defqt.bat
@@ -0,0 +1,5 @@
+@echo off
+rem Setting up environment for Qt usage...
+set PATH=C:\Qt\Qt5.9.9\5.9.9\msvc2015\bin;%PATH%
+rem cd /D C:\Qt\Qt5.9.9\5.9.9\msvc2015
+rem echo Remember to call vcvarsall.bat to complete environment setup!
